e90801bbf79aa53b29d4aed2f3cacb7e1ab4bf07
Leonardo Braga <braga0425@gmail.com>
2025-03-09 14:18:52 -0300

    Fix ways.

--- cache_set.py
+++ cache_set.py
@@ -1,25 +1,23 @@
 import random
 
-from way import Way
-
 
 class Cache_Set:
     def __init__(self, assoc, subst):
-        self.ways = [Way() for _ in range(assoc)]
+        self.ways = [{'tag': None, 'val': False} for _ in range(assoc)]
         self.subst = subst
         self.list = []
 
     def insert(self, tag):
         for i, way in enumerate(self.ways):
-            if way.is_val():
-                if way.get_tag() == tag:
+            if way['val']:
+                if way['tag'] == tag:
                     if self.subst == 'L':
                         self.list.remove(i)
                         self.list.append(i)
                     return 0
             else:
-                way.set_tag(tag)
-                way.set_val(True)
+                way['tag'] = tag
+                way['val'] = True
                 self.list.append(i)
                 return 1
 
@@ -27,12 +25,12 @@ class Cache_Set:
 
     def replace(self, tag):
         if self.subst == 'R':
-            n_via = random.randint(0, len(self.ways) - 1)
-            self.ways[n_via].set_tag(tag)
+            nway = random.randint(0, len(self.ways) - 1)
+            self.ways[nway]['tag'] = tag
             return 2
         elif self.subst == 'F' or self.subst == 'L':
-            n_via = self.list.pop(0)
-            self.list.append(n_via)
-            self.ways[n_via].set_tag(tag)
+            nway = self.list.pop(0)
+            self.list.append(nway)
+            self.ways[nway]['tag'] = tag
             return 2
         return -1
